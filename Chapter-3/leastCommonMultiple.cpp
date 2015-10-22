835a742e18b7309e398ae2379c412b2b5c24a204
floppypanda <wobougerdao@gmail.com>
2015-10-22 15:30:11 -0600

    new file:   Chapter-3/leastCommonMultiple.cpp 	new file:   Chapter-3/leastCommonMultiple.h 	modified:   Chapter-3/chapter3.h 	modified:   Chapter-3/greatestCommonDivisor.cpp 	modified:   Chapter-3/largestInteger.cpp 	modified:   Chapter-3/leastCommonMultiple.cpp 	modified:   Chapter-3/leastCommonMultiple.h 	modified:   Makefile 	modified:   main.cpp

---
+++ Chapter-3/leastCommonMultiple.cpp
@@ -0,0 +1,5 @@
+#include "leastCommonMultiple.h"
+
+//Problem Description (#13) - Given two positive integers,
+//find their least common multiple.
+
---
+++ Chapter-3/leastCommonMultiple.h
@@ -0,0 +1,5 @@
+#ifndef GCD_H
+#define GCD_H
+#include "greatestCommonDivisor.h"
+#endif
+
