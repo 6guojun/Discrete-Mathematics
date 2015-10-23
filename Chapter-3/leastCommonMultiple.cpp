2f409a8c55752930802fe44dad9b5be79bb466b9
floppypanda <wobougerdao@gmail.com>
2015-10-22 18:55:59 -0600

    modified:   leastCommonMultiple.cpp 	modified:   leastCommonMultiple.h 	modified:   chapter3.h 	modified:   greatestCommonDivisor.cpp 	modified:   largestInteger.cpp 	modified:   ../Makefile 	modified:   ../main.cpp

--- Chapter-3/leastCommonMultiple.cpp
+++ Chapter-3/leastCommonMultiple.cpp
@@ -1,5 +1,20 @@
 #include "leastCommonMultiple.h"
 
-//Problem Description (#13) - Given two positive integers,
+//Problem Description (#13) : Given two positive integers,
 //find their least common multiple.
 
+/*Returns the least common multiple given two positive integers.*/
+unsigned int leastCommonMultiple(unsigned int a, unsigned int b)
+{
+	return ((a*b)/greatestCommonDivisor(a, b));
+}
+
+/*Tests leastCommonMultiple().*/
+bool leastCommonMultipleTest()
+{
+	cout << "Least Common Multiple Test" << endl;
+	unsigned int a, b;
+	cout << "Enter two positive integers: ";
+	cin >> a >> b;
+	cout << "The least common multiple is " << leastCommonMultiple(a, b) << endl;
+}
--- Chapter-3/leastCommonMultiple.h
+++ Chapter-3/leastCommonMultiple.h
@@ -1,5 +1,13 @@
+#ifndef IOSTREAM_H
+#define IOSTREAM_H
+#include <iostream>
+#endif
 #ifndef GCD_H
 #define GCD_H
 #include "greatestCommonDivisor.h"
 #endif
 
+using namespace std;
+
+unsigned int leastCommonMultiple(int, int);
+bool leastCommonMultipleTest();
